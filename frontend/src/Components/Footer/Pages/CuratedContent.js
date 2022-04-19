fbf4f292188f6c0b6d3f10f2f84bdce6b1b184fa
ashish <raikwarashish1290@gmail.com>
2022-04-19 23:39:35 +0530

    curated content

--- frontend/src/Components/Footer/Pages/CuratedContent.js
+++ frontend/src/Components/Footer/Pages/CuratedContent.js
@@ -1,21 +1,22 @@
 import React, { useLayoutEffect } from "react";
+import '../../../App.css';
 
 const CuratedContent = () => {
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
   });
   return (
-    <div className="jadoo" style={{ marginTop: "7.5rem" }}>
+    <div className="jadoo loop" style={{ marginTop: "7.5rem" }}>
       <div
-        className="container"
-        style={{ textAlign: "justify", textJustify: "inter-word",fontFamily:"cursive" }}
+        className="container" 
+        style={{ textAlign: "justify", textJustify: "inter-word",fontFamily:"roboto" }}
       >
         <h1 className="text-center" style={{fontWeight:"bold"}}>Re-invent</h1>
-        <p className="mt-4" style={{margin:"1rem 7rem"}}>
+        <p className="mt-4"  >
           We all want to live better lives,have better things and create a
           better world but the real question is how?
         </p>
-        <p className="mt-4" style={{margin:"1rem 7rem"}}>
+        <p className="mt-4" >
           In such a fast paced world, we seldom think of our own roots. We have
           lost the very essence of this life and creation because we are always
           chasing something better or bigger. But what about this miraculous
@@ -23,7 +24,7 @@ const CuratedContent = () => {
           that you need for you to exist? What are we doing with this body and
           mind - <strong>the greatest creation of nature.</strong>
         </p>
-        <p className="mt-4" style={{margin:"1rem 7rem"}}>
+        <p className="mt-4" >
           Isn’t it quite rare that we pay attention to our own thoughts,
           emotions and actions, rather we are caught up in knowing others,
           checking feeds, reading news and everything that happens outside of
@@ -31,7 +32,7 @@ const CuratedContent = () => {
           controlled by what happens inside of you. The thoughts that we think
           and the emotions that we feel drive our actions.
         </p>
-        <p className="mt-4" style={{margin:"1rem 7rem"}}>
+        <p className="mt-4" >
           In today’s world, 70% of the people are stressed, anxious or depressed
           and the only reason is, they don’t pay attention to themselves. Take a
           moment and recall the most happiest moment in your life and you will
@@ -40,14 +41,14 @@ const CuratedContent = () => {
           feel emotionally good then it is very much possible that thinking sad
           thoughts would make you emotionally dull.
         </p>
-        <p className="mt-4" style={{margin:"1rem 7rem"}}>
+        <p className="mt-4" >
           As it has been said, <strong>“Practice makes a man perfect”</strong>, Rehearsing to
           think happy thoughts would make you perfect at feeling happy and
           energetic emotions. And choosing your thoughts and emotions makes you
           independent of everything and everyone. Once you have such a level of
           control over your mind and body, the magic will flow into your life.
         </p>
-        <p className="mt-4" style={{margin:"1rem 7rem"}}>
+        <p className="mt-4" >
           The only way to have control over your thoughts and emotions is to
           change your subconscious program. The subconscious program is nothing
           but the part of the brain that controls 95% of our actions like the
@@ -58,31 +59,31 @@ const CuratedContent = () => {
           these habits form the program and then it goes on autopilot where you
           keep thinking and feeling the same way all the time.
         </p>
-        <p className="mt-4" style={{margin:"1rem 7rem"}}>
+        <p className="mt-4" >
           The door to your subconscious mind opens right after you wake up and
           right before you go to bed because we have low frequency brain waves
           at those times of the day. This is the perfect time to change your
           subconscious program to build the habit of thinking, feeling and
           acting as you want yourself to. The formula is -<strong>MEDITATION</strong>.
         </p>
-        <p className="mt-4" style={{margin:"1rem 7rem"}}>
+        <p className="mt-4" >
           Meditation has been the core practice in ancient India and it has
           proved to improve the overall lives of people and, currently Science
           researched and used it to heal a lot of unfortunate people just by the
           practice of meditation.
         </p>
-        <p className="mt-4" style={{margin:"1rem 7rem"}}>
+        <p className="mt-4" >
           It is a technique to empty your mind and then rehearse in your mind
           the exact thoughts, emotions and actions. When you keep doing this
           consistently for a period of time, just like laziness was so
           effortless, being happy and healthy would become effortless.
         </p>
-        <p className="mt-4" style={{margin:"1rem 7rem"}}>
+        <p className="mt-4" >
           It not only helps you to rebuild yourself but it connects you to the
           very source of our creation and the nature of love. It is the only way
           of life, rest is just survival.
         </p>
-        <p className="mt-4" style={{margin:"1rem 7rem"}}><strong>Reconnect to reinvent..!!!</strong></p>
+        <p className="mt-4" ><strong>Reconnect to reinvent..!!!</strong></p>
       </div>
     </div>
   );
